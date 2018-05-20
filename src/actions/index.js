f3173324397f0315b02a87120d6cef5a10443301
Pierre Pellegrin <informatique@check-kit.com>
2018-05-20 13:57:34 +0200

    stuffs

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '011922ea2bee4e8f5e6e4acc2c156b48';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
